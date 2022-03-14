d632fb72f84d1e70c4b99f5d342f52de8d05b8c6
Kim Tae Hyun <97386129+tjdrkr2580@users.noreply.github.com>
2022-03-14 21:10:38 +0900

    3/14

--- login-form/app.js
+++ login-form/app.js
@@ -1,1 +1,11 @@
-const email = document
+const email = document.getElementById("user");
+const password = document.getElementById("pwd");
+const loginbutton = document.getElementById("login-button");
+const forgotpwd = document.getElementById("forgot-password");
+const loginform = document.querySelector("#login-form");
+const HIDDEN_NAMED = "hidden"; 
+function loginMyAcount() {
+    loginform.classList.add(HIDDEN_NAMED);
+}
+
+loginbutton.addEventListener("click", loginMyAcount);
